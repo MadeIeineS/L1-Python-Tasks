bfcd4de67cba3cda9c5ec116907068717ea64381
steedm2 <steedm2@stu.otc.school.nz>
2021-03-12 13:13:05 +1300

    Did a thing.

---
+++ Task 1.py
@@ -0,0 +1,16 @@
+#points scored
+match_1 = 3
+match_2 = 6
+match_3 = 10
+match_4 = 10
+match_5 = 6
+
+# calculation
+Total_points = match_1 + match_2 + match_3 + match_4 +match_5
+
+print(f"First match = {match_1}"
+      f" second match = {match_2}"
+      f" third match = {match_3} "
+      f"fourth match = {match_4} "
+      f"fifth match = {match_5}")
+print(f"This means that your total score is {Total_points}")
