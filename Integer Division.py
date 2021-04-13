2b8c9bbb826e301cd5acb975c0c90b7cda8e3ff8
steedm2 <steedm2@stu.otc.school.nz>
2021-04-13 14:59:02 +1200

    Did a thing.

---
+++ Integer Division.py
@@ -0,0 +1,16 @@
+chocolate_bars = 18
+people = 5
+bars_each = chocolate_bars // people
+extra_squares = bars_each * 7 //people
+leftover_squares = bars_each * 7 % people
+
+print(f"""Whole bars each: {bars_each}
+extra squares each: {extra_squares}
+leftover squares: {leftover_squares}""")
+
+
+
+
+
+
+
